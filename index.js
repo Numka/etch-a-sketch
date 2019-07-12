0bceea5cd526cc85d9dc6016b1a3b6cb68330377
Numinor <numinor93@mail.ru>
2019-07-12 20:47:21 +0300

    Minor completion of functionality

---
+++ index.js
@@ -0,0 +1,22 @@
+let num = 16;
+const target12 = document.querySelector("#container");
+
+function createGrid(num) {
+    for(let i = 1; i <= num; i++) {
+        for(let j = 1; j <= num; j++) {
+            target12.insertAdjacentHTML('afterbegin', `<div class="asd"></div>`);
+        }
+    }
+}
+
+target12.addEventListener("mouseover", (e) => {
+    if(e.target && e.target.className == "asd") {
+        e.target.classList.toggle('turnBlack');
+    }
+}) 
+
+function clear() {
+    
+}
+
+createGrid(num);
