ea8f77000dfb64c79aff782fa7c6e18b0e8f4b2d
Numinor <numinor93@mail.ru>
2019-07-14 18:58:00 +0300

    Finished functionality. Todo: css

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-let num = 9;
-const target12 = document.querySelector("#container");
+
+const data = {
+    num: 16
+}
+const DOMstrings = {
+    container: document.querySelector("#container")
+}
 
 function createGrid(num) {
+    //Number.isInteger(num) ? num : 16;
+    let cur = 100 / num;
     for(let i = 1; i <= num; i++) {
         for(let j = 1; j <= num; j++) {
-            target12.insertAdjacentHTML('afterbegin', `<div class="asd"></div>`);
+            DOMstrings.container.insertAdjacentHTML('afterbegin', `<div style="height: ${cur}%; width: ${cur}%" class="asd"></div>`);
         }
     }
 }
 
-target12.addEventListener("mouseover", (e) => {
+DOMstrings.container.addEventListener("mouseover", (e) => {
     if(e.target && e.target.className == "asd") {
         e.target.classList.toggle('turnBlack');
     }
 }) 
 
-function clear12() {
+function clear1() {
     let myNodeList = document.querySelectorAll(".asd");
     myNodeList.forEach(el => el.classList.remove('turnBlack'));
 }
 
-createGrid(num);
+function reset() {
+    let myNodeList = document.querySelectorAll(".asd");
+    myNodeList.forEach(el => DOMstrings.container.removeChild(el));
+    do {
+        data.num = parseInt(prompt("Input desired amount of squares per side(1-64):", 16), 10);
+    } while (!(data.num >= 1 && data.num <= 64)); // asd
+    createGrid(data.num);
+}
+
+(function(){
+    createGrid(data.num);
+})();
